d9e8ca402c817f55efafaf09bdd413ec1085d328
eveau <eve.audrey.ng@gmail.com>
2018-08-05 05:21:09 +0200

    premier fichier

---
+++ Applicatif/Demarrage.java
@@ -0,0 +1,35 @@
+package Applicatif;
+
+//import Service.JoueurService;
+//import Service.VerifInstallService;
+/**
+ * @author baboulou
+ * @param Appel
+ *            du Menu General. <br/>
+ * 			Version Master<br/>
+ *            Demande et saisit l'entrée de l'utilisateur
+ */
+public class Demarrage {
+	// propriétés de classe
+
+	// constructeur sans argument
+	public Demarrage() {
+
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Démarrage, jeu RPG");
+		// VerifInstallService.verifInitRep();
+		// JoueurService.connection();
+
+	}
+
+	// getter and setter
+
+	// methodes de classe
+
+	// override
+
+	// tostring
+}
