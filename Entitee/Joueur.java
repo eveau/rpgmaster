daa26131bff36f7ce05b39e3332969917ca73784
eveau <eve.audrey.ng@gmail.com>
2018-08-05 15:29:14 +0200

    ajout des entites

---
+++ Entitee/Joueur.java
@@ -0,0 +1,74 @@
+package Entitee;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Un Joueur peut avoir plusieurs parties.<br/>
+ * Version Master Un joueur peut avoir plusieurs persos. Chaque partie contient
+ * 1 seul perso.<br/>
+ *
+ * @author baboulou
+ *
+ */
+public class Joueur implements Serializable {
+	// propriétés de classe
+	private static final long serialVersionUID = 1L;
+	private String identifiant, mail, motDePasse;
+	boolean isidentifie = false;
+	// ArrayList<Personnage> vListe2persos;
+	List<Personnage> jListe2persos;
+
+	// List<Partie> vListe2parties;
+	// constructeur sans argument
+	public Joueur() {
+
+	}
+
+	// getter and setter
+	public String getIdentifiant()
+	{
+		return identifiant;
+	}
+
+	public void setIdentifiant(String identifiant)
+	{
+		this.identifiant = identifiant;
+	}
+
+	public String getMail()
+	{
+		return mail;
+	}
+
+	public void setMail(String mail)
+	{
+		this.mail = mail;
+	}
+
+	public String getMotDePasse()
+	{
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse)
+	{
+		this.motDePasse = motDePasse;
+	}
+
+	public boolean isIsidentifie()
+	{
+		return isidentifie;
+	}
+
+	public void setIsidentifie(boolean isidentifie)
+	{
+		this.isidentifie = isidentifie;
+	}
+
+	// methodes de classe
+
+	// override
+
+	// tostring
+}
---
+++ Entitee/Personnage.java
@@ -0,0 +1,35 @@
+package Entitee;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author baboulou
+ * @param Personnage
+ *            version Master<br/>
+ */
+public class Personnage implements Serializable {
+	// propriétés de classe
+	private static final long serialVersionUID = 1L;
+	Inventaire<Object> inventaire;
+	List<Object> sac;
+	Map<String, Integer> caracteristiques;
+	String nom, classe, aspect, genre = A;
+	int pointdeVie = 10, endurance = 10, pointMagie = 10;
+	Joueur jId;
+	public static final String M = "Masculin", F = "Feminin", TS = "Transgenre", A = "Agenree", AA = "autre";
+
+	// constructeur sans argument
+	public Personnage() {
+
+	}
+
+	// getter and setter
+
+	// methodes de classe
+
+	// override
+
+	// tostring
+}
